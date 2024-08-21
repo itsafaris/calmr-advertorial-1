618a3ab583cbdf2c2b7d4bbb6dfae0fee5e737b2
martynasj <martyjankauskas@gmail.com>
2024-08-21 16:25:22 +0300

    intro section

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 
 import { SEO } from "@components/SEO";
-import { Box, Container, Flex, Heading, Text } from "@chakra-ui/react";
+import { Badge, Box, Container, Flex, Heading, Stack, Text } from "@chakra-ui/react";
 import { siteConfig } from "src/conf";
 import { StaticImage } from "gatsby-plugin-image";
 
@@ -14,28 +14,70 @@ const IndexPage: React.FC<PageProps> = () => {
   return (
     <Box>
       <Container>
-        <Heading my={4}>Hey! I'm {siteConfig.personaName}</Heading>
-        <Text></Text>
-        <Flex flexWrap={"wrap"} gap={4}>
-          <StaticImage
-            alt="personal weight loss blogger picture"
-            src="../images/main-avatar.png"
-            height={200}
-            width={200}
-            style={{ flexShrink: 0, width: 160, height: 160 }}
-          />
-          <Text flex={1} minWidth={240}>
-            As a mother of three, navigating the constant demands of family life while transitioning
-            from my career as an elementary school teacher to a full-time wellness coach, finding an
-            effective weight loss method that could seamlessly fit into my busy routine was no easy
-            task. The daily pressures of parenting, coupled with the challenges of maintaining a
-            healthy lifestyle amidst school runs, meal preps, and family commitments, made it
-            difficult to stick to any weight management plan. Despite trying a variety of
-            solutions—from diet plans to exercise routines and natural supplements—nothing seemed to
-            truly work.
+        <Box my={4}>
+          <Heading textAlign={"center"}>Hey! I'm {siteConfig.personaName}</Heading>
+          <Flex flexWrap={"wrap"} justifyContent={"center"} my={2}>
+            <StaticImage
+              alt="personal weight loss blogger picture"
+              src="../images/main-avatar.png"
+              height={200}
+              width={200}
+              style={{ flexShrink: 0, width: 80, height: 80, borderRadius: "100%" }}
+            />
+          </Flex>
+          <Text fontSize={"md"} textAlign={"center"} px={4}>
+            A mother of three, ex elementary school teacher, now a full-time wellness blogger
           </Text>
-        </Flex>
-        <h1>Congratulations</h1>
+        </Box>
+
+        <Box my={10}>
+          <Heading as="h2" my={2} fontSize={"2xl"}>
+            My 3-year long weight loss journey... And how I finally conquered it
+          </Heading>
+          <Text fontSize={"sm"}>Updated by {siteConfig.personaName} 14 days ago</Text>
+          <Flex gap={2} my={2} flexWrap={"wrap"}>
+            <Badge colorScheme="blue">Weight loss experiments</Badge>
+            <Badge colorScheme="blue">3 years of research</Badge>
+            <Badge colorScheme="blue">Product comparison</Badge>
+          </Flex>
+
+          <Box mt={8}>
+            <StaticImage
+              alt="before and after weight loss journey"
+              src="../images/before-after.jpg"
+              height={600}
+              width={600}
+              style={{ flexShrink: 0 }}
+            />
+            <Flex mb={4}>
+              <Text flex={1} fontSize={"sm"}>
+                Before weight loss. 174 lbs. Mar 14, 2023
+              </Text>
+              <Text flex={1} fontSize={"sm"}>
+                Before weight loss. 174 lbs. Mar 14, 2023
+              </Text>
+            </Flex>
+
+            <Stack spacing={4}>
+              <Text>
+                As a mother of three, navigating the constant demands of family life while
+                transitioning from my career as an elementary school teacher to a full-time wellness
+                coach, finding an effective weight loss method that could seamlessly fit into my
+                busy routine was no easy task. The daily pressures of parenting, coupled with the
+                challenges of maintaining a healthy lifestyle amidst school runs, meal preps, and
+                family commitments, made it difficult to stick to any weight management plan.
+              </Text>
+              <Text>
+                Despite trying a variety of solutions—from diet plans to exercise routines and
+                natural supplements—nothing seemed to truly work.
+              </Text>
+              <Text>
+                But now, I'm thrilled to share that I've finally discovered a weight loss strategy
+                that genuinely works for me.
+              </Text>
+            </Stack>
+          </Box>
+        </Box>
       </Container>
     </Box>
   );
