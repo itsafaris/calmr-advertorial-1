ddd5e3c595e362104518f162044e8dbf711fc945
martynasj <martyjankauskas@gmail.com>
2024-08-22 15:56:11 +0300

    replace verdict

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -59,7 +59,7 @@ const IndexPage: React.FC<PageProps> = () => {
         <SupBurnXT />
         <SupStripfast />
         <SupVitauthority />
-        <SupNOW />
+        {/* <SupNOW /> */}
 
         <Verdict />
         <FullComparisonChart />
